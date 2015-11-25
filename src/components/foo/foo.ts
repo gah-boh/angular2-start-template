821a453ef06f3f3a9c7472eac10e5a5274c5b1a7
Mike Brocchi <mbrocchi@gmail.com>
2015-11-25 16:23:56 -0500

    Added play stuff input params and output events

--- src/components/foo/foo.ts
+++ src/components/foo/foo.ts
@@ -1,15 +1,21 @@
-import {Component, Input, NgModel} from 'angular2/angular2';
+import {Component, Input, Output, NgModel, EventEmitter} from 'angular2/angular2';
 
 @Component({
     selector: 'user',
     template: `
     <input [(ng-model)]="userData.firstName"/>
-    <div>{{userData.lastName}}</div>
+    <input [(ng-model)]="userData.lastName"/>
 	`,
   directives: [NgModel]
 })
 export class User {
-	constructor() {}
+	constructor() {
+    setInterval(() => {
+      var d = new Date();
+      this.letknow.next(d.toString());
+    }, 1000);
+  }
 
   @Input() public userData: any;
+  @Output() letknow = new EventEmitter();
 }
